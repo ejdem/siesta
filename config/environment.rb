67a76d4d35a15801a0d359ccbed92fd90f23bac6
ejdem <adam_krawiec93@o2.pl>
2015-11-16 16:58:37 +0000

    added sendgrid data

--- config/environment.rb
+++ config/environment.rb
@@ -3,14 +3,12 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
-=begin
 ActionMailer::Base.smtp_settings = {
-    :user_name => ENV['sendgrid_username'],
-    :password  => ENV['sendgrid_password'],
-    :domain    => 'siesta-adamkr-1.c9.io/',
-    :address   => 'smtp.sendgrid.net',
-    :port      => 587,
-    :authentication       => :plain,
-    :enable_starttls_auto => true  
+  :address        => 'smtp.sendgrid.net',
+  :port           => '587',
+  :authentication => :plain,
+  :user_name      => ENV['SENDGRID_USERNAME'],
+  :password       => ENV['SENDGRID_PASSWORD'],
+  :domain         => 'heroku.com',
+  :enable_starttls_auto => true
 }
-=end
