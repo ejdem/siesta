c3d56ed94fd025541d83de0acef7da2f2b2a4185
ejdem <adam_krawiec93@o2.pl>
2015-11-23 15:27:01 +0000

    adding notes works well ;)

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -10,11 +10,17 @@ class NotesController < ApplicationController
     
     def create
         @subject = Subject.find(params[:subject_id])
-        user    = User.find(params[:user_id])
-        @note   = user.notes.new(note_params)
-        @note.save!
-        flash[:success] = "note added"
-        redirect_to @subject
+        @user    = User.find(params[:user_id])
+        @note    = @user.notes.new(note_params)
+        @note.subject_id = @subject.id
+        if @note.save!
+            
+            flash[:success] = "note added"
+            redirect_to @subject
+        else
+            flash[:danger] = "note not added"
+            redirect_to @subject
+        end
     end
     
     private
--- db/migrate/20151116220943_create_notes.rb
+++ db/migrate/20151116220943_create_notes.rb
@@ -2,11 +2,9 @@ class CreateNotes < ActiveRecord::Migration
   def change
     create_table :notes do |t|
       t.float      :note
-      #t.string     :bywords
       t.integer    :subject_id
       t.integer    :user_id
       t.string     :author
-      #t.references :user, index: true, foreign_key: true
       t.timestamps null: false
     end
     
